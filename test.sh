f9c12ced24bfb87923d2f2fec548ac9766bfc406
Arto Vuori <arto@usetrace.com>
2014-08-28 12:58:50 +0300

    Run Usetrace dev tests.

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH=/tmp/jenkins-usetrace.sh
 if [ ! -f $SCRIPT_PATH ]; then
         curl -s http://usetrace.com/integration/jenkins-usetrace.sh > $SCRIPT_PATH
     fi
-    bash $SCRIPT_PATH U_11HP39Bf1Q80hb_f0a_fv9_Rt7 http://test.usetrace.com chrome,firefox smoke
+    bash $SCRIPT_PATH Umr9Hf1UVkMsAABGff15_f0bXij9HP39 http://test.usetrace.com?abtestoff=all chrome smoke
